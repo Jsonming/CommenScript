7ca7810715ab90ea89aeb86ca8cefadc35f4fc9a
jsonming <ymmbjcz@126.com>
2020-05-22 11:07:25 +0800

    代码更新

--- update_data/count_each_person.py
+++ update_data/count_each_person.py
@@ -1,7 +1,22 @@
 import os
 from pydub import AudioSegment
 import pandas as pd
+import logging
+logger = logging.getLogger("YMM")
+log_path = os.getcwd() + '/Logs/'
+n = 1
+log_name =log_path+'//'+str(n) + '-log.log'
+if not os.path.exists(log_path):
+    os.makedirs(log_path)
 
+while os.path.exists(log_name):
+    n += 1
+    log_name = log_path + str(n) + '-log.log'
+fh = logging.FileHandler(log_name, mode='a', encoding="utf8")
+console = logging.StreamHandler()
+console.setLevel(logging.WARNING)
+logger.addHandler(fh)
+logger.addHandler(console)
 
 def get_person_sum(wav_list):
     s = 0
@@ -53,7 +68,6 @@ def get_dir_list(work_dir):
             dir_list.append(path)
     return dir_list
 
-
 def check_person_info(wav_list):
     """
     检查一个人的meta信息是否一致
@@ -61,6 +75,15 @@ def check_person_info(wav_list):
     :return:
     """
     info_set = set()
+    meta_dict = get_meta_dict(wav_list[0].replace('.wav', '.metadata'))
+    sex = meta_dict['SEX']
+    age = meta_dict['AGE']
+    acc = meta_dict['ACC']
+    act = meta_dict['ACT']
+    bir = meta_dict['BIR']
+    mit = meta_dict['MIT']
+    first_info = (sex, age, acc, act, bir, mit)
+
     for wav_file in wav_list:
         meta_path = wav_file.replace('.wav', '.metadata')
         meta_dict = get_meta_dict(meta_path)
@@ -72,28 +95,43 @@ def check_person_info(wav_list):
         mit = meta_dict['MIT']
         info = (sex, age, acc, act, bir, mit)
         info_set.add(info)
-
-    if len(info_set) != 1:
-        raise Exception("个人meta信息不一致")
-    else:
-        return info_set.pop()
+        if not first_info == info:
+            logger.error(meta_path+'\t'+str(info)+'\tfirst:'+str(first_info))
+    return first_info
 
 
 if __name__ == '__main__':
-    work_dir = r'\\10.10.30.14\刘晓东\oracle_交付\apy161101028_g_351人意大利语手机采集语音数据\完整数据包_processed\data\category'
-    res_path = r'apy161101028_g_351人意大利语手机采集语音数据.xlsx'
+    # work_dir = r'\\10.10.30.14\刘晓东\oracle_交付\apy161101034_r_227小时西班牙语手机采集语音数据\data\category'
+    # res_path = r'\\10.10.30.14\刘晓东\oracle_交付\apy161101034_r_227小时西班牙语手机采集语音数据\apy161101034_r_227小时西班牙语手机采集语音数据.xlsx'
+    # work_dir = r'\\10.10.30.14\刘晓东\oracle_交付\apy170801048_338小时西班牙语手机采集语音数据\data\category'
+    # res_path = r'\\10.10.30.14\刘晓东\oracle_交付\apy170801048_338小时西班牙语手机采集语音数据\apy161101034_r_227小时西班牙语手机采集语音数据.xlsx'
+
+    # work_dir = r"\\10.10.30.14\刘晓东\数据分类\语音数据\apy161101032_r_199小时英式英语手机采集语音数据\完整数据包_processed\data\category"
+    # res_path = r'\\10.10.30.14\刘晓东\数据分类\语音数据\apy161101032_r_199小时英式英语手机采集语音数据\apy161101032_r_199小时英式英语手机采集语音数据.xlsx'
+
+    # work_dir = r"\\10.10.30.14\刘晓东\数据分类\语音数据\apy161101032_r_199小时英式英语手机采集语音数据\完整数据包_processed\data\category"
+    # res_path = r'\\10.10.30.14\刘晓东\数据分类\语音数据\apy161101032_r_199小时英式英语手机采集语音数据\apy161101032_r_199小时英式英语手机采集语音数据.xlsx'
+    # work_dir = r"\\10.10.30.14\刘晓东\数据分类\语音数据\apy161101032_g_357人英式英语手机采集语音数据\完整数据包_processed\data\category"
+    # res_path = r'\\10.10.30.14\刘晓东\数据分类\语音数据\apy161101032_g_357人英式英语手机采集语音数据\apy161101032_g_357人英式英语手机采集语音数据.xlsx'
+    # work_dir = r"\\10.10.30.14\apy161101005_245小时车载环境普通话手机采集语音数据\完整数据包_加密后数据\data\category"
+    # res_path = r'\\10.10.30.14\apy161101005_245小时车载环境普通话手机采集语音数据\apy161101005_245小时车载环境普通话手机采集语音数据.xlsx'
+
+    work_dir = r"\\10.10.30.14\apy161101014_r_662小时中文重口音手机采集语音数据\完整数据包_processed\data\category"
+    res_path = r'\\10.10.30.14\apy161101014_r_662小时中文重口音手机采集语音数据\apy161101014_r_662小时中文重口音手机采集语音数据.xlsx'
+
+    print(work_dir)
 
     data = []
     for person in get_dir_list(work_dir):
-        name = person.split('\\')[-1]
-        wav_list = get_file_list(person, '.wav')
         try:
+            name = person.split('\\')[-1]
+            wav_list = get_file_list(person, '.wav')
             info = check_person_info(wav_list)
+            wav_sum = get_person_sum(wav_list)
+            person_info = [name, *info,wav_sum]
+            data.append(person_info)
         except Exception as e:
-            raise Exception("{}meta信息不一致".format(name))
+            logger.error(person+'\t'+'other_error'+'\t'+str(e))
 
-        wav_sum = get_person_sum(wav_list)
-        person_info = [name, *info]
-        data.append(person_info)
-    df = pd.DataFrame(data, columns=['person_num', 'SEX', 'AGE', 'ACC', 'ACT', 'BIR', 'MIT'])
+    df = pd.DataFrame(data, columns=['person_num', 'SEX', 'AGE', 'ACC', 'ACT', 'BIR', 'MIT','wav_durtion'])
     df.to_excel(res_path, index=False)
--- update_data/vad.py
+++ update_data/vad.py
@@ -2,9 +2,12 @@
 # -*- coding: utf-8 -*-
 import collections
 import contextlib
+import json
 import os
+import sys
 import wave
 
+import array
 import webrtcvad
 
 mode = 3
@@ -158,7 +161,8 @@ def vad_collector(sample_rate, frame_duration_ms,
                 voiced_frames = []
         i = i + 1
     if triggered:
-        endpoint = frames[i - 1].timestamp - (padding_duration_ms / 1000)
+        # endpoint = frames[i - 1].timestamp - (padding_duration_ms / 1000)
+        endpoint = frames[i - 1].timestamp + frame_duration_ms / 1000
         # points.append((startpoint, endpoint))
         count = count + 1
         # sys.stdout.write('-(%s)' % (frames[i-1].timestamp + frames[i-1].duration))
@@ -178,8 +182,8 @@ def get_wav_time(wav_path):
         f = file.getparams().framerate  # 采样频率
         # sample_time = 1 / f  # 采样点的时间间隔
         time = a / f  # 声音信号的长度
-    except Exception:
-        print(wav_path+" open error")
+    except Exception as e:
+        print(wav_path + " open error")
         return 0
 
     return time
@@ -195,6 +199,16 @@ def get_all_file(rawdir):
     return allfile
 
 
+def cut_st_ed(frame_duration_ms, audio, sample_rate):
+    """Generates audio frames from PCM audio data.
+    Takes the desired frame duration in milliseconds, the PCM data, and
+    the sample rate.
+    Yields Frames of the requested duration.
+    """
+    n = int(sample_rate * (frame_duration_ms / 1000.0) * 2)
+    return audio[:n], audio[-n:]
+
+
 # 传入一个wav路径，返回静音段的开始结束时间点，以及语音总时长
 def get_wav_start_end(f):
     global mode
@@ -202,6 +216,7 @@ def get_wav_start_end(f):
         audio, sample_rate = read_wave(f)
     except Exception as e:
         print(e)
+        return 0, 0, 0, 0
     t_all = get_wav_time(f)
     vad = webrtcvad.Vad(mode)
     frames = frame_generator(30, audio, sample_rate)
@@ -214,7 +229,71 @@ def get_wav_start_end(f):
         if i == 0:
             start = segment[2][0]
         end = segment[2][1]
-    return start, end, t_all
+    return start, end, t_all, len(frames)
 
 
+def judge_clip(f):
+    global mode
+    try:
+        audio, sample_rate = read_wave(f)
+    except Exception as e:
+        print(e)
+        return 0, 0, 0, 0
+    st, ed = cut_st_ed(50, audio, sample_rate)
+    st_frames = [fa for fa in array.array('h', st) if fa > 600]
+    ed_frames = [fa for fa in array.array('h', ed) if fa > 600]
+    if len(st_frames) > 5 or len(ed_frames) > 5:
+        return True
+    return False
 
+
+def main(src, dst):
+    mute_length = 0.07
+    error_json = {}
+    total_json = {}
+    for root, dirs, filenames in os.walk(src):
+        for filename in filenames:
+            if filename.endswith('.wav'):
+                wav_path = os.path.join(root, filename)
+                start, end, duration, frames = get_wav_start_end(wav_path)
+                info = {
+                    filename.replace('.wav', ''):
+                        {'start': start,
+                         'end': end,
+                         'duration': duration
+                         }
+                }
+                if start < mute_length or duration - end < mute_length:
+                    error_json.update(info)
+                total_json.update(info)
+        print('handle {}'.format(root))
+    with open(os.path.join(dst, 'error.json'), 'w') as f:
+        f.write(json.dumps(error_json))
+
+    with open(os.path.join(dst, 'total.json'), 'w') as f:
+        f.write(json.dumps(total_json))
+
+
+def handle(src, dst):
+    error_json = {}
+    for root, dirs, filenames in os.walk(src):
+        for filename in filenames:
+            if filename.endswith('.wav'):
+                wav_path = os.path.join(root, filename)
+                if judge_clip(wav_path):
+                    error_json.update({filename: ''})
+                    # print(filename)
+                    # import shutil
+                    # shutil.move(wav_path, os.path.join(dst, filename))
+        print('handle {}'.format(root))
+    with open(os.path.join(dst, 'error_frame.json'), 'w') as f:
+        f.write(json.dumps(error_json))
+
+
+# src = sys.argv[1]
+# dst = sys.argv[2]
+# handle(src, dst)
+
+# src = r"\\10.10.30.14\刘晓东\数据分类\语音数据\apy161101032_g_357人英式英语手机采集语音数据\完整数据包_processed\data"
+# dst = r"D:\Workspace\CommenScript\update_data"
+# handle(src, dst)
